426dca56a2d1a934aab60a9c94aa5cea240576fc
ToasterTFG <chulumancongewana1@gmail.com>
2025-04-16 11:13:11 +0200

    Update CoursesServices.java

--- src/main/java/com/group14/SpringAssignment/CoursesServices.java
+++ src/main/java/com/group14/SpringAssignment/CoursesServices.java
@@ -1,6 +1,6 @@
-package com.group14.SpringAssignment;
+package com.group14.springassigment;
 
-
+import com.group14.springassigment.foundation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
@@ -9,8 +9,14 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/courses")
-public class CoursesServices {
+public class courseServices {
 
     // Initialize with three different arrays
-    private final List<Foundation> arrays = new ArrayList<>();
-}
+    private final List<foundation> arrays = new ArrayList<>();
+
+    public courseServices() {
+        // Initialize with 3 arrays containing 2, 4, and 5 items respectively
+        arrays.add(new foundation("Foundation", new String[]{"Algorithms and Programming", "Computing Systems" ,"Data and Analysis","Impacts of Computing","Networks and Internet"}));
+        arrays.add(new foundation("Undergraduate", new String[]{"Software engineering", "Data Science", "Computer programming", "Database Developer"}));
+        arrays.add(new foundation("Honours", new String[]{"Data analyst", "Artificial intelligence", "Machine learning", "Web Developer", "Data Science"}));
+    }
