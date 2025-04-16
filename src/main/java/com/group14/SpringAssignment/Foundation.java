979ae3e3f5db08579d6d2ab2d7a8bae65bac61b9
SamkeloMngqibisa <202250013@ufh.ac.za>
2025-04-16 21:41:45 +0200

    Update Foundation.java

--- src/main/java/com/group14/SpringAssignment/Foundation.java
+++ src/main/java/com/group14/SpringAssignment/Foundation.java
@@ -8,3 +8,38 @@ import org.springframework.web.bind.annotation.*;
 public class foundation {
     private String name;
     private String[] items;
+package com.group14.springassigment;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+public class foundation {
+    private String name;
+    private String[] items;
+
+    // Constructors
+    public foundation() {}
+
+    public foundation(String name, String[] items) {
+        this.name = name;
+        this.items = items;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getItems() {
+        return items;
+    }
+
+    public void setItems(String[] items) {
+        this.items = items;
+    }
+}
